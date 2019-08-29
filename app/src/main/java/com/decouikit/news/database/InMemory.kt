69e06b7dc402b8e03908cc2ecabb2b3115c4b50b
Dragan Koprena <>
2019-08-30 00:08:31 +0200

    add media, tag api

--- app/src/main/java/com/decouikit/news/database/InMemory.kt
+++ app/src/main/java/com/decouikit/news/database/InMemory.kt
@@ -1,13 +1,22 @@
 package com.decouikit.news.database
 
 import com.decouikit.news.network.dto.Category
+import com.decouikit.news.network.dto.MediaItem
+import com.decouikit.news.network.dto.Tag
 import com.decouikit.news.network.dto.User
 
 object InMemory {
     private var CATEGORY: List<Category> = arrayListOf()
-    private var USER: List<User> = arrayListOf()
     private var CATEGORY_MAP = mapOf<Int, Category>()
+
+    private var MEDIA: List<MediaItem> = arrayListOf()
+    private var MEDIA_MAP = mapOf<Int, MediaItem>()
+
+    private var USER: List<User> = arrayListOf()
     private var USER_MAP = mapOf<Int, User>()
+
+    private var TAGS: List<Tag> = arrayListOf()
+    private var TAGS_MAP = mapOf<Int, Tag>()
 
     fun setCategoryList(categoryList: List<Category>) {
         CATEGORY = categoryList
@@ -18,6 +27,15 @@ object InMemory {
 
     fun getCategoryList(): List<Category> = CATEGORY
 
+    fun setTagsList(tagsList: List<Tag>) {
+        TAGS = tagsList
+        if (TAGS.isNotEmpty()) {
+            TAGS.forEach { TAGS_MAP.plus((Pair(it.id, it))) }
+        }
+    }
+
+    fun getTagsList(): List<Tag> = TAGS
+
     fun setUserList(userList: List<User>) {
         USER = userList
         if (USER.isNotEmpty()) {
@@ -27,11 +45,28 @@ object InMemory {
 
     fun getUserList(): List<User> = USER
 
+    fun setMediaList(mediaList: List<MediaItem>) {
+        MEDIA = mediaList
+        if (MEDIA.isNotEmpty()) {
+            MEDIA.forEach { MEDIA_MAP.plus((Pair(it.id, it))) }
+        }
+    }
+
+    fun getMediaList(): List<MediaItem> = MEDIA
+
     fun getCategoryById(categoryId: Int): Category? {
         return CATEGORY_MAP[categoryId]
     }
 
     fun getUserById(userId: Int): User? {
         return USER_MAP[userId]
+    }
+
+    fun getMediaById(mediaId: Int): MediaItem? {
+        return MEDIA_MAP[mediaId]
+    }
+
+    fun getTagById(tagId: Int): Tag? {
+        return TAGS_MAP[tagId]
     }
 }
