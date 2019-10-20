f5c0a656f4280ab1356187139eb67d5521b974b8
radovan.bogdanic <radovan.bogdanic@comtrade.com>
2019-10-20 21:55:30 +0200

    Added Image desc.

--- app/src/main/java/com/decouikit/news/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/decouikit/news/adapters/ViewPagerAdapter.kt
@@ -1,12 +1,13 @@
 package com.decouikit.news.adapters
 
+
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
-
 import java.util.ArrayList
 
-class ViewPagerAdapter(manager: FragmentManager) : FragmentStatePagerAdapter(manager) {
+class ViewPagerAdapter(manager: FragmentManager) :
+    FragmentStatePagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     private val mFragmentList = ArrayList<Fragment>()
 
     override fun getItem(position: Int): Fragment {
